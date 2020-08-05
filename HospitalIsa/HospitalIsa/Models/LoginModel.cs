278ea629da179112d5ed7e1fb89dcf70d4ba1ed8
ivkovicboris <borisivkovic95@gmail.com>
2020-08-05 22:10:08 +0200

    Models for Login and Register

---
+++ HospitalIsa/HospitalIsa/Models/LoginModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalIsa.API.Models
+{
+    public class LoginModel
+    {
+    }
+}
